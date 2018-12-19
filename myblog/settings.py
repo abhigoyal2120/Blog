4ccf16660003d452a3b071489f12a539da3efb38
abhijeet <abhigoyal2120@gmail.com>
2018-12-19 12:43:26 +0530

    added join for templates

--- myblog/settings.py
+++ myblog/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/hp/project/practice/blogProject/blog/myblog/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
